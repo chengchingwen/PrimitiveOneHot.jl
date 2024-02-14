3d62844515ad31cb4fe70b526be0feac752a38ef
chengchingwen <chengchingwen214@gmail.com>
2024-02-14 20:31:02 +0800

    update doc script

--- docs/make.jl
+++ docs/make.jl
@@ -5,12 +5,11 @@ DocMeta.setdocmeta!(PrimitiveOneHot, :DocTestSetup, :(using PrimitiveOneHot); re
 
 makedocs(;
     modules=[PrimitiveOneHot],
-    authors="chengchingwen <adgjl5645@hotmail.com> and contributors",
-    repo="https://github.com/chengchingwen/PrimitiveOneHot.jl/blob/{commit}{path}#{line}",
+    authors="chengchingwen <chengchingwen214@gmail.com> and contributors",
     sitename="PrimitiveOneHot.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://chengchingwen.github.io/PrimitiveOneHot.jl",
+        edit_link="main",
         assets=String[],
     ),
     pages=[
@@ -20,4 +19,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/chengchingwen/PrimitiveOneHot.jl",
+    devbranch="main",
 )
